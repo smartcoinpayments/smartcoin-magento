03d92cc07c3c5b33b81153e66458b9e59d7b6416
Arthur Granado <agranado@smartcoin.com.br>
2015-05-23 20:34:11 -0300

    Include methods to get Bank Slip URL and Bar Code

--- app/code/community/Smartcoin/Smartcheckout/Block/Success/Payment/Bs.php
+++ app/code/community/Smartcoin/Smartcheckout/Block/Success/Payment/Bs.php
@@ -9,5 +9,11 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Smartcoin_Smartcheckout_Block_Checkout_Success_Payment_Bs extends Smartcoin_Smartcheckout_Block_Checkout_Success_Payment_Default {
-	
+	public function getSmartcoinBankSlipLink() {
+    return $this->getInfo()->getSmartcoinBankSlipLink();
+  }
+
+  public function getSmartcoinBankSlipBarCode() {
+    return $this->getInfo()->getSmartcoinBankSlipBarCode();
+  }
 }
