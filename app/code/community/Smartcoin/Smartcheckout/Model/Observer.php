0ea117f8eeef8439dbfb97ba1ec754096bc5bbac
Ricardo Caldeira <ricardo.nezz@gmail.com>
2015-05-28 20:40:12 -0300

    Added Smartcoin.js to all pages

--- app/code/community/Smartcoin/Smartcheckout/Model/Observer.php
+++ app/code/community/Smartcoin/Smartcheckout/Model/Observer.php
@@ -10,27 +10,6 @@
  */
 
 class Smartcoin_Smartcheckout_Model_Observer {
-  public function addSmartcoinJavascriptBlock(Varien_Event_Observer $observer) {
-      /** @var $block Mage_Core_Block_Abstract */
-    $block = $observer->getEvent()->getBlock();
-    $blockType = $block->getType();
-    $targetBlocks = array(
-        'checkout/onepage_payment',
-        'aw_onestepcheckout/onestep_form_paymentmethod',
-    );
-    
-    if (in_array($blockType, $targetBlocks)) {
-      /** @var $transport Varien_Object */
-      $transport = $observer->getEvent()->getTransport();
-      $html = $transport->getHtml();
-      $preHtml = $block->getLayout()
-      ->createBlock('core/template')
-      ->setTemplate('smartcoin/checkout/js.phtml')
-      ->toHtml();
-      $transport->setHtml($preHtml . $html);
-    }
-  }
-
   public function addCheckoutJavascriptBlock(Varien_Event_Observer $observer) {
       /** @var $block Mage_Core_Block_Abstract */
     $block = $observer->getEvent()->getBlock();
